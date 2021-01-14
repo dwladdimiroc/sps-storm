ca4d07e4f5b7738f8793ba42d74fac195c61366f
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2021-01-14 11:18:57 +0100

    Utils for write file and parse string

---
+++ util/file.go
@@ -0,0 +1,88 @@
+package util
+
+import (
+	//		"fmt"
+	"bytes"
+	"io/ioutil"
+	"regexp"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func writeFile(data []byte, filename string) {
+	folder := "stats/"
+	errWrite := ioutil.WriteFile(folder+filename, data, 0644)
+	check(errWrite)
+}
+
+func ParseCPU(dataFile string, components string) {
+	var buffer bytes.Buffer
+
+	lines := strings.Split(dataFile, "\n")
+	lines = lines[2 : len(lines)-1]
+
+	for index, line := range lines {
+		regular, errRegular := regexp.Compile(`\s+`)
+		check(errRegular)
+		line = regular.ReplaceAllString(line, " ")
+		words := strings.Split(line, " ")
+
+		for index, word := range words {
+			if index != len(words)-1 {
+				buffer.WriteString(word)
+				buffer.WriteString(";")
+			} else {
+				buffer.WriteString(word)
+			}
+		}
+
+		if index != len(lines)-1 {
+			buffer.WriteString("\n")
+		}
+	}
+
+	filename := "statsCPU-" + components + ".csv"
+	writeFile(buffer.Bytes(), filename)
+
+	buffer.Reset()
+
+}
+
+func ParseMemory(dataFile string, components string) {
+	var buffer bytes.Buffer
+
+	lines := strings.Split(dataFile, "\n")
+	lines = lines[1 : len(lines)-1]
+
+	for index, line := range lines {
+		regular, errRegular := regexp.Compile(`\s+`)
+		check(errRegular)
+		line = regular.ReplaceAllString(line, " ")
+		line = strings.TrimSpace(line)
+		words := strings.Split(line, " ")
+
+		for index, word := range words {
+			if index != len(words)-1 {
+				buffer.WriteString(word)
+				buffer.WriteString(";")
+			} else {
+				buffer.WriteString(word)
+			}
+		}
+
+		if index != len(lines)-1 {
+			buffer.WriteString("\n")
+		}
+	}
+
+	filename := "statsMem-" + components + ".csv"
+	writeFile(buffer.Bytes(), filename)
+
+	buffer.Reset()
+
+}

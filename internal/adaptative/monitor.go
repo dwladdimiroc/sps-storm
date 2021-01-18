7d52102f13efdfed5182603a59fe9f8c71822b8c
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2021-01-18 23:41:36 +0100

    Change package name

--- internal/adaptative/monitor.go
+++
@@ -1,8 +0,0 @@
-package adaptative
-
-const STORM_API_URL string = "127.0.0.1"
-const STORM_API_PORT int = 80
-
-func initMonitor() {
-
-}
--- internal/adaptative/system.go
+++
@@ -1,9 +0,0 @@
-package adaptative
-
-func start() {
-
-}
-
-func stop() {
-
-}

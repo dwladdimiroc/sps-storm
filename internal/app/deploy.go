b03aaf38e57aca19ad80a4045e5a08dc0a31bcff
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2021-01-29 11:58:52 +0100

    Change minor: cmd params read from config file

--- internal/app/deploy.go
+++ internal/app/deploy.go
@@ -1,12 +1,18 @@
 package app
 
 import (
+	"github.com/dwladdimiroc/sps-storm/internal/storm"
 	"github.com/dwladdimiroc/sps-storm/internal/util"
+	"github.com/spf13/viper"
 )
 
-func Deploy() {
+const DirCmd = "scripts"
+
+func Deploy() string {
 	appCmdStormApp := "sh"
-	argsCmdStormApp := []string{"startApp.sh"}
-	dirCmdStormApp := "/home/daniel/storm/projects"
+	argsCmdStormApp := []string{viper.GetString("storm.deploy.script")}
+	dirCmdStormApp := DirCmd
 	util.Execute(appCmdStormApp, argsCmdStormApp, dirCmdStormApp)
+	topologyId := storm.GetTopologyId()
+	return topologyId
 }

6d5f8ed791f05f814dffbb1794edc792d3f758b9
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2021-02-21 12:54:06 +0100

    Change Execute Model for rebalance storm to redis configuration

--- internal/adaptive/execute.go
+++ internal/adaptive/execute.go
@@ -3,19 +3,22 @@ package adaptive
 import (
 	"fmt"
 	"github.com/dwladdimiroc/sps-storm/internal/storm"
+	"github.com/dwladdimiroc/sps-storm/internal/util"
 	"log"
+	"strconv"
 	"strings"
 )
 
 func execute(topology storm.Topology) {
-	params := parseParamsRebalanced(topology)
-	ok := storm.Rebalanced(topology.Id, params)
-	log.Printf("execute: rebalanced topolog %v\n", ok)
+	//params := parseParamsRebalanced(topology)
+	//ok := storm.Rebalanced(topology.Id, params)
+	err := updateReplicas(topology)
+	log.Printf("execute: rebalanced topolog %v\n", err)
 	period = 0
 }
 
 func parseParamsRebalanced(topology storm.Topology) string {
-	var base = "{\"rebalanceOptions\" : {\"executors\" : {TOPOLOGY_STORM}}, \"callback\" : \"foo\"}"
+	var base = "{\"rebalanceOptions\" : {\"executors\" : {TOPOLOGY_Sfor i, bolt := range topology.Bolts {TORM}}, \"callback\" : \"foo\"}"
 	var executors string
 	for i, bolt := range topology.Bolts {
 		var paramBolt string
@@ -28,4 +31,17 @@ func parseParamsRebalanced(topology storm.Topology) string {
 	}
 	params := strings.Replace(base, "TOPOLOGY_STORM", executors, 1)
 	return params
+}
+
+func updateReplicas(topology storm.Topology) error {
+	var err error
+	for _, bolt := range topology.Bolts {
+		value := strconv.FormatInt(bolt.Replicas, 10)
+		if errRedis := util.RedisSet(bolt.Name, value); errRedis != nil {
+			log.Printf("update replicas error: %v\n", errRedis)
+			err = errRedis
+		}
+	}
+
+	return err
 }
--- internal/util/cache.go
+++ internal/util/cache.go
@@ -15,12 +15,34 @@ func RedisFlush() error {
 	rdb := redis.NewClient(&redis.Options{
 		Addr: addr,
 	})
+	defer rdb.Close()
 
 	ctx := context.Background()
+	defer ctx.Done()
 	if val, err := rdb.FlushAll(ctx).Result(); err != nil {
 		return err
 	} else {
 		log.Printf("redis flushall: %v\n", val)
+		return nil
+	}
+}
+
+func RedisSet(key, value string) error {
+	host := viper.GetString("redis.host")
+	port := viper.GetString("redis.port")
+	addr := host + ":" + port
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: addr,
+	})
+	defer rdb.Close()
+
+	ctx := context.Background()
+	defer ctx.Done()
+	if val, err := rdb.Set(ctx, key, value, 0).Result(); err != nil {
+		return err
+	} else {
+		log.Printf("redis set: %v\n", val)
 		return nil
 	}
 }

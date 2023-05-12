d20f6bd499eca11f6adea732564cdddafd5600e2
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2023-05-12 18:36:06 +0200

    New improvement for ANN

--- py/ann.py
+++ py/ann.py
@@ -4,14 +4,16 @@ from sklearn.neural_network import MLPRegressor
 
 
 def ann_prediction(samples, prediction_number):
+    for i in range(len(samples)):
+        samples[i] /= 1000
+
     t = list(range(len(samples)))
     # print(t)
-    # print(samples)
-    mlp = MLPRegressor(random_state=1, max_iter=500).fit(np.array(t).reshape(-1, 1), samples)
+    mlp = MLPRegressor(random_state=1, max_iter=1000).fit(np.array(t).reshape(-1, 1), samples)
 
     predictions = []
     for x in range(prediction_number):
-        predictions.append(mlp.predict(np.array([len(samples) + x]).reshape(-1, 1))[0])
+        predictions.append(mlp.predict(np.array([len(samples) + x]).reshape(-1, 1))[0]*1000)
         # print(f"predicted f({len(samples) + x}): {mlp.predict(np.array([len(samples) + x]).reshape(-1, 1))}")
 
     next_input = np.average(predictions)

f89235efc19e2c058cd1596d81bab3bedc4e70c8
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2024-09-20 21:11:03 +0200

    Add new params in the script args

--- internal/app/deploy.go
+++ internal/app/deploy.go
@@ -4,13 +4,14 @@ import (
 	"github.com/dwladdimiroc/sps-storm/internal/storm"
 	"github.com/dwladdimiroc/sps-storm/internal/util"
 	"github.com/spf13/viper"
+	"strconv"
 )
 
 const DirCmd = "scripts"
 
 func Deploy() string {
 	appCmdStormApp := "sh"
-	argsCmdStormApp := []string{viper.GetString("storm.deploy.script")}
+	argsCmdStormApp := []string{viper.GetString("storm.deploy.script"), viper.GetString("storm.deploy.dataset"), strconv.Itoa(viper.GetInt("storm.adaptive.limit_replicas"))}
 	dirCmdStormApp := DirCmd
 	util.Execute(appCmdStormApp, argsCmdStormApp, dirCmdStormApp)
 	topologyId := storm.GetTopologyId()

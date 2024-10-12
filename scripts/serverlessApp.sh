e7ba864aae18f9777b255368b4c7782209722509
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2024-10-12 18:50:22 +0200

    New params in Storm App script

--- scripts/serverlessApp.sh
+++ scripts/serverlessApp.sh
@@ -1,2 +1,2 @@
-/home/daniel/storm/2.6.2/bin/storm kill -w 0 serverlessApp
-/home/daniel/storm/2.6.2/bin/storm jar serverlessApp.jar com.github.dwladdimiroc.normalApp.topology.Topology constant 10
+/home/daniel/storm/2.6.2/bin/storm kill -w 0 linearApp
+/home/daniel/storm/2.6.2/bin/storm jar serverlessApp.jar com.github.dwladdimiroc.serverlessApp.topology.LoadTopology $1 $2

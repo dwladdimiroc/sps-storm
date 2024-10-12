e4c9f77648dd6da8739c1c14a7f511b2b6cd0b92
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2024-10-12 18:53:00 +0200

    New concurrency and implementation of the new interval time of Plan module

--- internal/predictive/prediction.go
+++ internal/predictive/prediction.go
@@ -3,7 +3,6 @@ package predictive
 import (
 	"github.com/dwladdimiroc/sps-storm/internal/storm"
 	"github.com/spf13/viper"
-	"log"
 	"math"
 	"sync"
 )
@@ -26,7 +25,7 @@ func GetPred() PredictionInput {
 }
 
 func InitPrediction() {
-	var basic, lr, fft, ann, rf, svm, bayesian, ridge, gaussian, sgd PredictionInput
+	var basic, lr, fft, ann, rf, svm, bayesian, ridge, sgd PredictionInput
 	//Basic
 	basic.NameModel = "basic"
 	basic.PredictedInput = make([]float64, viper.GetInt("storm.adaptive.analyze_samples"))
@@ -60,29 +59,48 @@ func InitPrediction() {
 	ridge.PredictedInput = make([]float64, viper.GetInt("storm.adaptive.analyze_samples"))
 	predictions = append(predictions, ridge)
 	//Gaussian
-	gaussian.NameModel = "gaussian"
-	gaussian.PredictedInput = make([]float64, viper.GetInt("storm.adaptive.analyze_samples"))
-	predictions = append(predictions, gaussian)
-	//MPL (ANN)
+	//gaussian.NameModel = "gaussian"
+	//gaussian.PredictedInput = make([]float64, viper.GetInt("storm.adaptive.analyze_samples"))
+	//predictions = append(predictions, gaussian)
+	//SGD
 	sgd.NameModel = "sgd"
 	sgd.PredictedInput = make([]float64, viper.GetInt("storm.adaptive.analyze_samples"))
 	predictions = append(predictions, sgd)
-}
 
-func PredictInput(topology *storm.Topology, period int) {
-	predictions[0].PredictedInput = append(predictions[0].PredictedInput, Simple(topology)...)
-
-	var wg sync.WaitGroup
-
-	for i := 1; i < len(predictions); i++ {
-		wg.Add(1)
-		go GetPredictionInput(topology, i, &wg, period)
+	if viper.GetString("storm.adaptive.predictive_model") != "multi" {
+		for i := 0; i < len(predictions); i++ {
+			if predictions[i].NameModel == viper.GetString("storm.adaptive.predictive_model") {
+				var singlePrediction = predictions[i]
+				predictions = nil
+				predictions = append(predictions, singlePrediction)
+			}
+		}
 	}
-
-	wg.Wait()
 }
 
-func GetPredictionInput(topology *storm.Topology, indexPredictor int, wg *sync.WaitGroup, period int) {
+func PredictInput(topology *storm.Topology) {
+	if len(predictions) == 1 {
+		if viper.GetString("storm.adaptive.predictive_model") == "basic" {
+			predictions[0].PredictedInput = append(predictions[0].PredictedInput, Simple(topology)...)
+		} else {
+			var wg sync.WaitGroup
+			wg.Add(1)
+			go GetPredictionInput(topology, 0, &wg)
+			wg.Wait()
+		}
+	} else {
+		predictions[0].PredictedInput = append(predictions[0].PredictedInput, Simple(topology)...)
+		var wg sync.WaitGroup
+		for i := 1; i < len(predictions); i++ {
+			wg.Add(1)
+			go GetPredictionInput(topology, i, &wg)
+		}
+
+		wg.Wait()
+	}
+}
+
+func GetPredictionInput(topology *storm.Topology, indexPredictor int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	var samples []float64
@@ -96,9 +114,11 @@ func GetPredictionInput(topology *storm.Topology, indexPredictor int, wg *sync.W
 		//log.Printf("analyze: train: index={%d},sample={%v},\n", i, topology.InputRate[i])
 	}
 
+	//log.Printf("[t=X] predict input : init prediction")
 	resultsPrediction := GetPrediction(samples, viper.GetInt("storm.adaptive.prediction_number"), predictions[indexPredictor].NameModel)
-	log.Printf("[t=%d] analyze: get prediction={%s}: samples={%v},lenSamples={%d},prediction={%v},lenPrediction={%d}\n", period, predictions[indexPredictor].NameModel, samples, len(samples), resultsPrediction, len(resultsPrediction))
-	predictions[indexPredictor].PredictedInput = append(predictions[indexPredictor].PredictedInput, resultsPrediction...)
+	if len(resultsPrediction) > 0 {
+		predictions[indexPredictor].PredictedInput = append(predictions[indexPredictor].PredictedInput, resultsPrediction...)
+	}
 }
 
 func DeterminatePredictor(topology *storm.Topology) {
@@ -107,38 +127,35 @@ func DeterminatePredictor(topology *storm.Topology) {
 
 	//Determinate the best predictor
 	indexPredictor := 0
-	minErrorEstimation := predictions[indexPredictor].ErrorEstimation
+	minErrorEstimation := predictions[0].ErrorEstimation
+	//log.Printf("[t=X] model={%s},RMSE={%.2f}", predictions[0].NameModel, predictions[0].ErrorEstimation)
 	for i := 1; i < len(predictions); i++ {
+		//log.Printf("[t=X] model={%s},RMSE={%.2f}", predictions[i].NameModel, predictions[i].ErrorEstimation)
 		if predictions[i].ErrorEstimation < minErrorEstimation {
 			indexPredictor = i
 			minErrorEstimation = predictions[i].ErrorEstimation
 		}
 	}
-
-	if viper.GetString("storm.adaptive.predictive_model") == "multi" {
-		indexChosenPredictor = indexPredictor
-	} else {
-		for i := 0; i < len(predictions); i++ {
-			if predictions[i].NameModel == viper.GetString("storm.adaptive.predictive_model") {
-				indexChosenPredictor = i
-				return
-			}
-		}
-	}
+	indexChosenPredictor = indexPredictor
 }
 
 // RMSE
 func calculateError(input []int64) {
-	for k := 0; k < len(predictions); k++ {
+	var timeWindow = viper.GetInt("storm.adaptive.analyze_samples") + viper.GetInt("storm.adaptive.prediction_number")
+	for p := 0; p < len(predictions); p++ {
 		var errorEst float64
-		for i := len(input) - viper.GetInt("storm.adaptive.prediction_number"); i < len(input); i++ {
-			errorEst += math.Pow(predictions[k].PredictedInput[i]-float64(input[i]), 2.0)
+		//log.Printf("[t=X] model={%s},lenPrediction={%d},lenInput={%d}", predictions[p].NameModel, len(predictions[p].PredictedInput), len(input))
+		for i := len(input) - timeWindow; i < len(input); i++ {
+			errorEst += math.Pow(predictions[p].PredictedInput[i]-float64(input[i]), 2.0)
 		}
-		predictions[k].ErrorEstimation = math.Sqrt(errorEst / float64(viper.GetInt("storm.adaptive.prediction_number")))
+		predictions[p].ErrorEstimation = math.Sqrt(errorEst / float64(timeWindow))
 	}
 }
 
 func GetPredictedInputPeriod(period int) int64 {
+	if period >= len(predictions[indexChosenPredictor].PredictedInput) {
+		period = len(predictions[indexChosenPredictor].PredictedInput) - 1
+	}
 	predictedInputPeriod := int64(predictions[indexChosenPredictor].PredictedInput[period])
 	//log.Printf("predicted input period : %d perdiction={%v}", period, predictions[indexChosenPredictor])
 	return predictedInputPeriod
